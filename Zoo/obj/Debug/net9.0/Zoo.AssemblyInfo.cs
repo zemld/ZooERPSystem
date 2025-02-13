fb76bdd3b7abb4e2433574ec852f8baad025fd38
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-02-13 10:27:19 +0300

    Установлена библиотека для тестов

--- Zoo/obj/Debug/net9.0/Zoo.AssemblyInfo.cs
+++ Zoo/obj/Debug/net9.0/Zoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9619c7ca1f0b502d67abe15dcc15f45b5dcae37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
